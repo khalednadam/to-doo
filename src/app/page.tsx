ad048b649a16510837f177f0d14f758ae51a7f0d
Khaled Nadam <khalednadam@gmail.com>
2023-08-26 00:17:53 +0300

    style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
 		<>
 			<header className="flex justify-between items-center mb-4">
 				<h1 className="text-3xl">To-Doo</h1>
-				<Link className="border px-2 py-1 rounded-lg hover:bg-slate-700 transition-all hover:shadow-xl shadow-white duration-300 outline-none focus-within:bg-slate-600"
+				<Link className="border hover:shadow-gray-200 hover:shadow-2xl transition-all duration-150 px-2 py-1 rounded-lg hover:bg-slate-700 outline-none focus-within:bg-slate-600"
 				href="/new">New</Link>
 			</header>
 			<ul>
